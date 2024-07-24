34a12fa9a11034b0435febac7185ab939838c257
haleemaraza <haleemasadiadeveloper@gmail.com>
2024-07-24 16:11:52 +0500

    done

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -9,8 +9,8 @@ export default function CartProvider({ children }: { children: ReactNode }) {
          mode="payment"
          cartMode='client-only'
          stripe={process.env.NEXT_PUBLIC_STRIPE_KEY as string}
-         successUrl="https://ecommerce-n5d6.vercel.app/stripe/success"
-         cancelUrl="https://ecommerce-n5d6.vercel.app/stripe/error"
+         successUrl="https://ecommerce-peach-delta.vercel.app/stripe/success"
+         cancelUrl="https://ecommerce-peach-delta.vercel.app/stripe/error"
          currency='USD'
          billingAddressCollection={true}
          shouldPersist={true}
